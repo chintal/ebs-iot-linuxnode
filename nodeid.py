6db9e1fbd73eb2012521758e3b197b8f09d300c0
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2018-05-06 18:36:16 +0530

    Made netifaces node id getter more robust.

--- nodeid.py
+++ nodeid.py
@@ -9,6 +9,7 @@ from .widgets import ColorLabel
 class NodeIDMixin(object):
     _node_id_getter = "uuid"
     _node_id_params = {}
+    _node_id_netifaces_fallback_interfaces = ['wlp1s0', 'wlan0', 'eth0']
 
     def __init__(self, *args, **kwargs):
         super(NodeIDMixin, self).__init__(*args, **kwargs)
@@ -24,15 +25,26 @@ class NodeIDMixin(object):
         getter = "_get_node_id_{0}".format(self._node_id_getter)
         return getattr(self, getter)(**self._node_id_params).upper()
 
-    @staticmethod
-    def _get_node_id_uuid(**_):
+    def _get_node_id_uuid(self, **_):
         node_id = uuid.getnode()
         if (node_id >> 40) % 2:
             raise OSError("The system does not seem to have a valid MAC")
         return hex(node_id)[2:]
 
-    @staticmethod
-    def _get_node_id_netifaces(interface):
+    def _get_node_id_netifaces_guess(self):
+        fallback_interfaces = self._node_id_netifaces_fallback_interfaces
+        available_interfaces = netifaces.interfaces()
+        default_gateway = netifaces.gateways()['default']
+        if default_gateway:
+            return default_gateway[netifaces.AF_INET][1]
+        for iface in fallback_interfaces:
+            if iface in available_interfaces:
+                return iface
+
+    def _get_node_id_netifaces(self, **kwargs):
+        interface = kwargs.get('interface', None)
+        if interface is None:
+            interface = self._get_node_id_netifaces_guess()
         mac = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
         return mac.replace(':', '')
 
