f64edce0a068b9904b44699d62993fa298322d05
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-01-04 03:30:06 +0530

    Change to simplified sidebar structure hooks. Tweak to allow orientation control.

--- ebs/iot/linuxnode/gallery.py
+++ ebs/iot/linuxnode/gallery.py
@@ -403,19 +403,24 @@ class GalleryGuiMixin(GalleryMixin, BaseGuiMixin):
     def gui_gallery_parent(self):
         if not self._gallery_parent_layout:
             self._gallery_parent_layout = RelativeLayout()
-            self.gui_sidebar_right.add_widget(self._gallery_parent_layout)
+            self.gui_sidebar.add_widget(self._gallery_parent_layout)
         return self._gallery_parent_layout
 
     def _gui_gallery_sidebar_control(self, *args):
         if self.gui_gallery.visible:
-            self.gui_sidebar_right_show('gallery')
+            self.gui_sidebar_show('gallery')
         else:
-            self.gui_sidebar_right_hide('gallery')
+            self.gui_sidebar_hide('gallery')
 
     @property
     def gui_gallery(self):
         if not self._gallery:
-            self._gallery = ImageGallery(parent_layout=self.gui_gallery_parent)
+            params = {'parent_layout': self.gui_gallery_parent}
+            if self.config.portrait:
+                params['animation_vector'] = (1, 0)
+            else:
+                params['animation_vector'] = (0, 1)
+            self._gallery = ImageGallery(**params)
             self._gallery.bind(visible=self._gui_gallery_sidebar_control)
         return self._gallery
 
